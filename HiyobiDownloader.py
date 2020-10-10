0a3d0bb903336474de33e28a2a51cfd2cab7896b
samsoon984 <entks82137@naver.com>
2020-10-10 15:30:01 +0900

    Update HiyobiDownloader.py

--- HiyobiDownloader.py
+++ HiyobiDownloader.py
@@ -2,40 +2,53 @@ from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 import requests
 import os
+import sys
 
-
-def get_url(number = int(input('품번 입력: ')), page = int(input('쪽수 입력: '))):
+def get_url(number = int(input('품번 입력: '))):
     #while i <= 10:
     #    print("https://hitomi.la/reader/%d.html#%d"%(number,i))
     #    i+=1
     #url1 = "https://hiyobi.me/reader/%d#1"%number
     i = 1
-    #path = "C:/Bitnami/wampstack-7.4.10-0/apache2/htdocs/learn/%s"%number
+    path1 = "C:/Bitnami/wampstack-7.4.10-0/apache2/htdocs/learn/%s"%number
+    path2 = "C:/Bitnami/wampstack-7.4.10-0/apache2/htdocs/learn"
     try:
         if not(os.path.isdir("%s"%number)):
             os.makedirs(os.path.join("%s"%number))
             print("making directory..")
     except:
         print("error!!!")
-    while i <= page:
+    while i <= 1000:
         if i < 10:
             if os.path.isdir("%s"%number):
-                url = "https://cdn.hiyobi.me/data/%s/0%s.png"%(number,i)
+                url = "https://cdn.hiyobi.me/data/%d/0%d.jpg"%(number,i)
                 res = requests.get(url)
-                file = open("%s.png"%i,"wb")
-                file.write(res.content)
-                file.close()
-                print(url)
-                i += 1
+                print(res.status_code)
+                if (res.status_code == 200):
+                    os.chdir(path1)
+                    file = open("%s.png"%i,"wb")
+                    file.write(res.content)
+                    file.close()
+                    print(url)
+                    os.chdir(path2)
+                    i += 1
+                else:
+                    sys.exit()
         else:
             if os.path.isdir("%s"%number):
-                url = "https://cdn.hiyobi.me/data/%s/%s.png"%(number,i)
+                url = "https://cdn.hiyobi.me/data/%d/%d.jpg"%(number,i)
                 res = requests.get(url)
-                file = open("%s.png"%i,"wb")
-                file.write(res.content)
-                file.close()
-                print(url)
-                i += 1
+                print(res.status_code)
+                if (res.status_code == 200):
+                    os.chdir(path1)
+                    file = open("%s.png"%i,"wb")
+                    file.write(res.content)
+                    file.close()
+                    print(url)
+                    os.chdir(path2)
+                    i += 1
+                else:
+                    sys.exit()
 
 
     #url1 = "https://github.com/samsoon984"
