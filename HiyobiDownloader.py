ad25cd6ded698a3f53780ed560b911d9446d6cab
samsoon984 <entks82137@naver.com>
2020-10-14 00:00:04 +0900

    Update HiyobiDownloader.py

--- HiyobiDownloader.py
+++ HiyobiDownloader.py
@@ -1,5 +1,5 @@
-from bs4 import BeautifulSoup
-from html.parser import HTMLParser
+#from bs4 import BeautifulSoup
+#from html.parser import HTMLParser
 import requests
 import os
 import sys
@@ -7,59 +7,33 @@ import time
 import random
 
 
-i = 1
-
 start = time.time()
 
 def get_url(number = int(input('품번 입력: '))):
-    path1 = "./%s"%number
-    path2 = "../"
-    def get_num(q: int):
-        global i
-        url = "https://cdn.hiyobi.me/data/%d/%s%d.jpg"%(number,q,i)
+    try:
+        if not(os.path.isdir(f"{number}")):
+            os.makedirs(os.path.join(f"{number}"))
+            print("making directory..")
+    except:
+        print("already exist..")
+    os.chdir(f"./{number}")
+    i = 1
+    while i <= 1000:
+        url = f"https://cdn.hiyobi.me/data/{number}/{str(i).zfill(2)}.jpg"
         res = requests.get(url)
         print(url)
         print(res.status_code)
         if (res.status_code == 200):
-            os.chdir(path1)
-            file = open("%s.png"%i, "wb")
-            file.write(res.content)
-            file.close()
-            os.chdir(path2)
+            with open(f"{i}.jpg", "wb") as f:
+                f.write(res.content)
+            f.close()
             i += 1
             print("sleep,,, this is not error")
             time.sleep( random.uniform(0.5,1) )
         else:
             print("time :", time.time() - start)
             sys.exit()
-    #while i <= 10:
-    #    print("https://hitomi.la/reader/%d.html#%d"%(number,i))
-    #    i+=1
-    #url1 = "https://hiyobi.me/reader/%d#1"%number
-    try:
-        if not(os.path.isdir("%s"%number)):
-            os.makedirs(os.path.join("%s"%number))
-            print("making directory..")
-    except:
-        print("error!!!")
-    while i <= 1000:
-        if i < 10:
-            if os.path.isdir("%s"%number):
-                get_num("0")
-        else:
-            if os.path.isdir("%s"%number):
-                get_num("")
-
-
-    #url1 = "https://github.com/samsoon984"
-    #res = requests.get(url1)
-    #res.raise_for_status()
-    #soup = BeautifulSoup(res.text, 'html.parser')
-    #print(soup)
-
-
-
-
+    
 get_url()
 
 
