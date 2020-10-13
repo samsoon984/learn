ba777b2fd245a487c7579b41ea5a638621465539
samsoon984 <entks82137@naver.com>
2020-10-14 00:10:39 +0900

    Update HiyobiDownloader.py

--- HiyobiDownloader.py
+++ HiyobiDownloader.py
@@ -1,5 +1,3 @@
-#from bs4 import BeautifulSoup
-#from html.parser import HTMLParser
 import requests
 import os
 import sys
@@ -11,12 +9,11 @@ start = time.time()
 
 def get_url(number = int(input('품번 입력: '))):
     try:
-        if not(os.path.isdir(f"{number}")):
+        if not(os.path.isdir(number)):
             os.makedirs(os.path.join(f"{number}"))
             print("making directory..")
     except:
         print("already exist..")
-    os.chdir(f"./{number}")
     i = 1
     while i <= 1000:
         url = f"https://cdn.hiyobi.me/data/{number}/{str(i).zfill(2)}.jpg"
@@ -24,9 +21,8 @@ def get_url(number = int(input('품번 입력: '))):
         print(url)
         print(res.status_code)
         if (res.status_code == 200):
-            with open(f"{i}.jpg", "wb") as f:
+            with open(f"{number}/{i}.jpg", "wb") as f:
                 f.write(res.content)
-                f.close()
             i += 1
             print("sleep,,, this is not error")
             time.sleep( random.uniform(0.5,1) )
