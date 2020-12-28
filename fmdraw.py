494c2ac48e37574160bfbaec8682926c1db2fe38
samsoon984 <entks82137@naver.com>
2020-12-29 02:03:45 +0900

    Create fmdraw.py

---
+++ fmdraw.py
@@ -0,0 +1,21 @@
+import requests
+import random
+from bs4 import BeautifulSoup
+
+#test url: https://www.fmkorea.com/3266529210
+
+url = input("url 입력( 예시: https://www.fmkorea.com/123456 ): ")
+req = requests.get(url)
+html = req.text
+soup = BeautifulSoup(html, 'html.parser')
+getComments = soup.select('#cmtPosition > ul')
+firstfind = soup.find('ul', {'class' :'fdb_lst_ul'}).find_all('a', {'href' :'#popup_menu_area'})
+cutOverlap = list(set(firstfind))
+listnum = 0
+print('----------------------------------')
+for i in cutOverlap:
+    print(cutOverlap[listnum].text)
+    listnum += 1
+print('----------------------------------')
+print(f"총 {listnum}명, 당첨자: {random.choice(cutOverlap).text}")
+print('----------------------------------')
