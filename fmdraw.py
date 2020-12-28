06f45713b087a8c89a4a99ec4fafb9276dd177b8
samsoon984 <entks82137@naver.com>
2020-12-29 04:29:06 +0900

    Update fmdraw.py
    이전에는 한페이지의 댓글만 불러왔지만
    이제 모든 댓글을 불러올 수 있게됨

--- fmdraw.py
+++ fmdraw.py
@@ -2,14 +2,21 @@ import requests
 import random
 from bs4 import BeautifulSoup
 
-#test url: https://www.fmkorea.com/3266529210
+#test url: https://www.fmkorea.com/3283165045
 
 url = input("url 입력( 예시: https://www.fmkorea.com/123456 ): ")
+urlNum = url.replace('https://www.fmkorea.com/','')
 req = requests.get(url)
 html = req.text
 soup = BeautifulSoup(html, 'html.parser')
-getComments = soup.select('#cmtPosition > ul')
-find = soup.find('ul', {'class' :'fdb_lst_ul'}).find_all('a', {'href' :'#popup_menu_area'})
+find = []
+if soup.find('strong', {'class': 'this'}).text != '1':
+    for i in range(1, int(soup.find('strong', {'class': 'this'}).text)+1):
+        commentUrl = f'https://www.fmkorea.com/index.php?document_srl={urlNum}&cpage={i}#{urlNum}_comment'
+        req = requests.get(commentUrl)
+        html = req.text
+        soup = BeautifulSoup(html, 'html.parser')
+        find += soup.find('ul', {'class' :'fdb_lst_ul'}).find_all('a', {'href' :'#popup_menu_area'})
 cutOverlap = list(set(find))
 listnum = 0
 print('----------------------------------')
